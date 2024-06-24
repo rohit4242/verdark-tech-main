be16762be133e630cbfdd06f774154f91f2ca515
Rohit luni <rohitluni4242@gmail.com>
2024-06-24 17:26:29 +0530

    contact details complete

--- src/app/contact-details/raju-kumar-singh/page.tsx
+++ src/app/contact-details/raju-kumar-singh/page.tsx
@@ -1,19 +1,42 @@
-import Heading from "@/components/Heading";
+"use client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { FC } from "react";
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
+  const handleSaveContact = () => {
+    const vcard = `BEGIN:VCARD
+    VERSION:3.0
+    FN:Raju Kumar Singh
+    TITLE:Business Development Manager
+    TEL;TYPE=CELL:+971508201569
+    TEL;TYPE=WORK:+971508201569
+    EMAIL;TYPE=WORK:vedarksouq@gmail.com
+    URL;TYPE=Website:https://vedarktech.com
+    ADR;TYPE=WORK:;;1351, burjuman business tower, burjuman, bur Dubai;UAE
+    END:VCARD`;
+
+    const blob = new Blob([vcard], { type: "text/vcard;charset=utf-8" });
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "raju_kumar_singh.vcf");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <div className="mb-20 ">
       <section className="bg-slate-950">
         <div className="py-2 lg:py-2 mb-8 lg:mb-2 px-4 mx-auto max-w-screen-md">
           <div className="mx-auto max-w-4xl text-center mt-14">
             <h1 className=" text-lg font-semibold leading-7 text-gradient">
-              Contact Us
+              Contact Details
             </h1>
             <p className="mt-2 text-4xl tracking-tight font-extrabold  text-white">
-            Raju Kumar Singh
+              Raju Kumar Singh
             </p>
             <div className="w-[22rem] md:w-[40rem] mx-auto h-2 mt-4 relative">
               {/* Gradients */}
@@ -25,67 +48,52 @@ const page: FC<pageProps> = ({}) => {
               {/* Radial Gradient to prevent sharp edges */}
               <div className="absolute inset-0 w-full h-full bg-slate-950 [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
             </div>
-            <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-slate-300">
-            Share Your Questions or Feedback
+            <p className="mx-auto mt-2 max-w-2xl text-center text-lg leading-8 text-slate-300">
+              Business Development Manager
             </p>
           </div>
 
-          <form action="#" className="space-y-8 mt-10">
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-300"
-              >
-                Your email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="shadow-sm 0 border   text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
-                placeholder="name@gmail.com"
-                required
-              />
+          <div className=" text-white mt-10 space-y-4 ">
+            <div className="flex justify-center items-center gap-1">
+              <p>Mobile Number: </p>
+              <Link href={"tel:971508201569"} className="hover:underline">
+                +971 50 820 1569
+              </Link>
             </div>
-            <div>
-              <label
-                htmlFor="subject"
-                className="block mb-2 text-sm font-medium text-gray-300"
+            <div className="flex justify-center items-center gap-1">
+              <p>Whatsapp Number: </p>
+              <Link
+                href={"https://wa.me/971508201569"}
+                className="hover:underline"
               >
-                Subject
-              </label>
-              <input
-                type="text"
-                id="subject"
-                className="block p-3 w-full text-sm  rounded-lg border  shadow-sm focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
-                placeholder="Let us know how we can help you"
-                required
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="message"
-                className="block mb-2 text-sm font-medium  text-gray-400"
+                +971 50 820 1569
+              </Link>
+            </div>{" "}
+            <div className="flex justify-center items-center gap-1">
+              <p>Email Address: </p>
+              <Link
+                href={"mailto:vedarksouq@gmail.com"}
+                className="hover:underline"
               >
-                Your message
-              </label>
-              <textarea
-                id="message"
-                className="block p-2.5 w-full text-sm  rounded-lg shadow-sm border  focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500"
-                placeholder="Leave a comment..."
-              ></textarea>
+                vedarksouq@gmail.com
+              </Link>
             </div>
-
-            <button
-              type="submit"
-              className="border text-sm font-medium w-full my-4 relative border-neutral-200 border-white/[0.2]  text-white px-6 py-4 rounded-full"
-            >
-              Send message
-              <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-            </button>
-          </form>
+            <div className="flex justify-center items-center gap-1">
+              <p>Telegram: </p>
+              <Link href={"https://t.me/vedark"} className="hover:underline">
+                @Vedark
+              </Link>
+            </div>
+            <div className="flex justify-center items-center gap-1">
+              <p>Office Address: </p>
+              <p>1351, burjuman business tower, burjuman, bur Dubai, UAE</p>
+            </div>
+            <div className="flex justify-center items-center">
+              <Button onClick={handleSaveContact}>Save Contact</Button>
+            </div>
+          </div>
         </div>
       </section>
-
     </div>
   );
 };
