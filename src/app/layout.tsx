f422877b269e3799204a11c765b21d0842018d82
therencho <me@therencho.com>
2024-10-19 02:48:34 +0530

    alpha 2.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer";
 import { ToastProvider } from "@/providers/toast-provider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { NavBar } from "@/components/NavBar";
-import { MarketingConfig } from "@/types";
+import { marketingConfig } from "@/config/marketing";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,30 +14,6 @@ export const metadata: Metadata = {
   description: "A Platform that provides Tailored AI bot Solutions",
 };
 
-export const marketingConfig: MarketingConfig = {
-  mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
-    {
-      title: "About Us",
-      href: "/about",
-    },
-    {
-      title: "Blog",
-      href: "/blogs",
-    },
-    {
-      title: "Pricing",
-      href: "/pricing",
-    },
-    {
-      title: "Contact",
-      href: "/contact",
-    }
-  ],
-};
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -47,9 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NavBar items={marketingConfig.mainNav} scroll={true} />
-
         <ToastProvider />
-
         {children}
         <Footer />
         <SpeedInsights />
---
+++ src/config/marketing.ts
@@ -0,0 +1,24 @@
+export const marketingConfig = {
+    mainNav: [
+      {
+        title: "Home",
+        href: "/",
+      },
+      {
+        title: "About Us",
+        href: "/about",
+      },
+      {
+        title: "Blog",
+        href: "/blogs",
+      },
+      {
+        title: "Pricing",
+        href: "/pricing",
+      },
+      {
+        title: "Contact",
+        href: "/contact",
+      },
+    ],
+  };
